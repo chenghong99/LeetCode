38ab8eff81a5b3427faa6037845cd63c8371a26c
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2023-10-12 11:34:02 +0800

    Time: 35 ms (48.61%), Space: 20.3 MB (69.32%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head == None:
+            return False
+        
+        for i in range(10001):
+            if head.next == None:
+                return False
+            else:
+                head = head.next
+                
+        return True
+        
