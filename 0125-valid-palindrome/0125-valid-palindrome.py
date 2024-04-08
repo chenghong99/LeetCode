10cb96f1bbb005b84a43c2fbd79a180b42074e87
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2024-04-08 12:09:13 +0800

    Time: 23 ms (87.52%), Space: 14.2 MB (16.37%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -4,15 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        s = s.lower()
-        s_arr = [char for char in s if char.isalnum()]
+        arr = [elem.lower() for elem in s if elem.isalnum()]
+        rev_arr = arr[::-1]
         
-        for i in range(len(s_arr) // 2):
-            
-            if s_arr[i] != s_arr[len(s_arr) - 1 - i]:
-                return False
-            
-        return True
+        return arr == rev_arr
         
         
         
