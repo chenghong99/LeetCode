83fea9e868145bc426e3cf9199960a2bfcda365f
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2024-04-17 20:16:06 +0800

    Time: 891 ms (38.46%), Space: 31.7 MB (18.11%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def checkSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        
+        dic = {0 : -1}
+        curr_sum = 0
+        
+        for i in range(len(nums)):
+            curr_sum += nums[i]
+            mod = curr_sum % k
+            
+            if dic.get(mod) == None:
+                dic[mod] = i
+                
+            elif i - dic.get(mod) > 1:
+                return True
+                
+        return False
+ 
+
+## prefix sum 
