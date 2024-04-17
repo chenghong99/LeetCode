3948002512a45b4bae85416165646c8a2835960e
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2024-04-17 17:50:40 +0800

    Time: 224 ms (62.38%), Space: 14.2 MB (80.72%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -7,17 +7,19 @@ class Solution(object):
         """
         
         dic = {0:1}
-        ans = 0
         curr_sum = 0
+        ans = 0
         
-        for i in nums: 
+        for i in nums:
             curr_sum += i
             if dic.get(curr_sum - k) != None:
                 ans += dic.get(curr_sum - k)
+                
             dic[curr_sum] = dic.get(curr_sum, 0) + 1
+                
         return ans
                 
-            
+
         
 ## prefix sum 
 
