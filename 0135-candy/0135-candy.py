4102f6418dffba30d620acde7d77a8ab7930df75
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2024-11-18 18:11:30 +0800

    Time: 27 ms (12.08%), Space: 19 MB (59.77%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -1,19 +1,20 @@
 class Solution:
     def candy(self, ratings: List[int]) -> int:
-        right = [1 for i in range(len(ratings))]
-        left = [1 for i in range(len(ratings))]
+        ## create an array whr all kids get 1 -> iterate front and back -> for each iteartion we compare the ratings to update the num of candies 
+        
+        arr_left = [1 for i in range(len(ratings))]
+        arr_right = [1 for i in range(len(ratings))]
         
         for i in range(1, len(ratings)):
-            if ratings[i] > ratings[i - 1]:
-                right[i] = right[i - 1] + 1
+            if ratings[i] > ratings[i-1]:
+                arr_right[i] = arr_right[i-1] + 1
                 
-        for i in range(len(ratings) - 2, -1, -1):
-            if ratings[i] > ratings[i + 1]:
-                left[i] = left[i + 1] + 1
-                
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                arr_left[i] = arr_left[i+1] + 1
+        
         candies = 0
-        for i in range(len(ratings)):
-            candies += max(right[i], left[i])
+        for i in range(len(arr_left)):
+            candies += max(arr_left[i], arr_right[i])
             
-        return candies 
-            
+        return candies
