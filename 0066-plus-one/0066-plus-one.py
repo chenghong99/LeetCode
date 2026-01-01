e71dc72e441d091025caffb79deed6c53e540634
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-01-01 17:50:02 +0800

    Time: 0 ms (100%), Space: 12.5 MB (52.05%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,19 +1,20 @@
-class Solution:
-    def plusOne(self, digits: List[int]) -> List[int]:
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
         
-        counter = 0
-        pos = len(digits) - 1
-        
-        while digits[pos] == 9:
-            digits[pos] = 0
-            counter += 1
-            pos -= 1
-            
-        if counter == len(digits):
+        ## iterate from the back, keep adding 1 until the sum does not lead to a 10. 
+
+        curr = len(digits) - 1
+        while curr >= 0:
+            if (digits[curr] + 1) % 10 == 0:
+                digits[curr] = 0
+                curr -= 1
+            else:
+                digits[curr] += 1
+                break 
+        if digits[curr] == 0:
             digits.insert(0, 1)
-            
-        else:
-            digits[pos] += 1
         return digits
-            
-        
