37a674bb444c39e346dbb1beffbc6fb77fcdacaf
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-01-08 20:28:47 +0800

    Time: 397 ms (10.54%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,12 +1,12 @@
 # Write your MySQL query statement below
-
-## under manager id more than 5 ppl 
-## get the manager id of those with more than 5 and match with employee table
-
-SELECT e2.name
-FROM Employee e2
-WHERE e2.id IN 
-(SELECT e1.managerId 
-FROM Employee e1
-GROUP BY e1.managerId 
-HAVING COUNT(e1.managerId) >= 5)
+with manager_id as (
+    select 
+    managerId,
+    count(*) as manager_count
+    from Employee
+    group by 1
+    having count(*) >= 5) 
+    
+    select name
+    from Employee a
+    inner join manager_id b on a.id = b.managerId
