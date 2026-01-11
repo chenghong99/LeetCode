b134d922e0ac7fcf5f1ca972d378e179634b7bcb
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-01-11 14:50:37 +0800

    Time: 33 ms (45.15%), Space: 21.7 MB (21.11%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,15 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        ## prefix sum qn. curr sum = x, find number of curr_sum - k that occurs before curr 
+        hashmap = {0:1} ## initialise with 0:1 for case where first item = k
         curr_sum = 0
-        prefix_sum = {0:1}
-        total = 0
-        
+        output = 0
+
         for i in nums:
-            curr_sum += i
-            total += prefix_sum.get(curr_sum - k, 0)
-            prefix_sum[curr_sum] = prefix_sum.get(curr_sum, 0) + 1
-            
-            
-        return total
-                 
+            curr_sum += i ## get curr_sum at current position
+            output += hashmap.get(curr_sum - k, 0) ## curr_sum - (curr_sum - k) = k
+            hashmap[curr_sum] = hashmap.get(curr_sum, 0) + 1
+
+        return output
+
         
