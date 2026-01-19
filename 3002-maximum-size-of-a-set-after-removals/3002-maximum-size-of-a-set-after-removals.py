0638b3a73de6c7d8a6bcf9f5ef060e8c243a7667
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-01-19 22:18:22 +0800

    Time: 27 ms (85.94%), Space: 28.9 MB (25%) - LeetHub

---
+++ 3002-maximum-size-of-a-set-after-removals/3002-maximum-size-of-a-set-after-removals.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maximumSetSize(self, nums1: List[int], nums2: List[int]) -> int:
+        ## convert both to sets, find overlap between both, take those elem from the set of shorter length. 
+        nums1_set = set(nums1)
+        nums2_set = set(nums2)
+
+        overlap = list(nums1_set.intersection(nums2_set))
+
+        remaining_set1 = min(len(nums1_set) - len(overlap), len(nums1) // 2)
+        remaining_set2 = min(len(nums2_set) - len(overlap), len(nums2) // 2)
+        overlap_set1 = min((len(nums1) // 2 - remaining_set1), len(overlap))
+        overlap_set2 = min((len(nums2) // 2 - remaining_set2), len(overlap) - overlap_set1)
+        return remaining_set1 + remaining_set2 + overlap_set1 + overlap_set2
+
+      
