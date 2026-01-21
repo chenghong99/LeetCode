97f062c3945e9d2cc4cf8be659149386f29a5c9c
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-01-21 08:41:49 +0800

    Time: 35 ms (39.92%), Space: 36.7 MB (29.91%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.py
@@ -1,22 +1,10 @@
-class Solution(object):
-    def containsNearbyDuplicate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
-        """
-        
-        hm = dict()
-        
-        for i in range(len(nums)):
-            if hm.get(nums[i]) == None:
-                hm[nums[i]] = i
-                
-            elif i - hm.get(nums[i]) <= k:
-                return True
-            
-            else:
-                hm[nums[i]] = i
-                
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        ## store the num in hashmap with their respective positions 
+        hashmap = {} ## store latest num pos
+
+        for pos, num in enumerate(nums):
+            if hashmap.get(num) != None and pos - hashmap.get(num) <= k:
+                return True 
+            hashmap[num] = pos 
         return False
-            
