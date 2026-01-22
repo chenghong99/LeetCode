3cb4d7fde89935ee55c7cb278adfaf0179d97f3d
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-01-22 13:19:40 +0800

    Time: 0 ms (100%), Space: 21.6 MB (8.07%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,43 +1,21 @@
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        """
-        :type intervals: List[List[int]]
-        :type newInterval: List[int]
-        :rtype: List[List[int]]
-        """
-        if len(intervals) == 0:
-            intervals.append(newInterval)
-            return intervals
-        
-        new_list = []
-        
-        
-        for i in range(len(intervals)):
-            if newInterval[0] <= intervals[i][0] <= newInterval[1] or newInterval[0] <= intervals[i][1] <= newInterval[1]:
-                if len(new_list) > 0:
-                    if newInterval[0] <= new_list[-1][0] <= newInterval[1] or newInterval[0] <= new_list[-1][1] <= newInterval[1]:
-                        new_list[-1][1] = max(intervals[i][1], newInterval[1])
-                    else:
-                        new_list.append([min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])]) 
-                else:
-                    new_list.append([min(intervals[i][0], newInterval[0]), max(intervals[i][1], newInterval[1])])
-            
-            else:
-                new_list.append(intervals[i])
-                
-        del intervals[:]
-        
-        for i in new_list:
-            intervals.append(i)
-        x = 0
-        for i in range(len(intervals)):
-            if intervals[i][0] <= newInterval[0] <= intervals[i][1] or intervals[i][0] <= newInterval[1] <= intervals[i][1]:
-                x = 1
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        ## insert and merge
+
+        interval_len = len(intervals)
+        for pos, interval in enumerate(intervals):
+            if interval[0] > newInterval[0]:
+                intervals.insert(pos, newInterval)
                 break
-        if x == 0:
+
+        if interval_len == len(intervals):
             intervals.append(newInterval)
-            intervals.sort()
-            
-        return intervals
-                
-        
+
+        new_interval = [intervals[0]]
+
+        for i in range(1, len(intervals)):
+            if new_interval[-1][1] >= intervals[i][0]:
+                new_interval[-1][1] = max(intervals[i][1], new_interval[-1][1])
+            else:
+                new_interval.append(intervals[i])
+        return new_interval
