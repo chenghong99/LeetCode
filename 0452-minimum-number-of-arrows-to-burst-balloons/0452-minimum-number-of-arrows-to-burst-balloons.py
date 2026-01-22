42bf8347b5cbecc3290f7427e41dea5df9516f53
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-01-22 14:19:11 +0800

    Time: 55 ms (98.06%), Space: 53.5 MB (24.51%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.py
@@ -2,8 +2,8 @@ class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         ## sort by start, find overlap and non overlap region and determine how many balloons to shoot 
         points.sort(key = lambda x: x[1])
-        output = 1
-        prev = points[0][1]
+        output = 0
+        prev = -10000000000000000
 
         for start, end in points:
             if start > prev:
