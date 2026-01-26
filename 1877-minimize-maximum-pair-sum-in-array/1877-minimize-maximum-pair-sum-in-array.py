13d246a6c825fc79227b7a4531edd58d07f28e97
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-01-27 07:58:15 +0800

    Time: 841 ms (75.82%), Space: 34.3 MB (6.45%) - LeetHub

--- 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
+++ 1877-minimize-maximum-pair-sum-in-array/1877-minimize-maximum-pair-sum-in-array.py
@@ -4,8 +4,7 @@ class Solution:
         nums.sort()
         min_max = 0
         for i in range(len(nums)//2):
-            curr_pair = nums[i] + nums[len(nums) - i - 1]
-            min_max = max(min_max, curr_pair)
+            min_max = max(min_max, nums[i] + nums[len(nums) - i - 1])
 
         return min_max
         
