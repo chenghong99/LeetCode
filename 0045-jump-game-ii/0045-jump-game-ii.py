a3e398802e102adce7dfed15f16eb231581e334d
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-08-23 13:01:51 +0800

    Time: 11 ms (36.26%), Space: 13 MB (79.9%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -4,22 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        jumps = 0
-        index = 0
-        
-        if len(nums) == 1:
-            return 0
-            
-        while index + nums[index] < len(nums) - 1:
-            max_distance = 0
-            curr_distance = 0
-            curr_index = 0
-            for i in range(1, nums[index] + 1):
-                curr_distance = index + i + nums[index + i]
-                if max_distance < curr_distance:
-                    max_distance = curr_distance
-                    curr_index = i
-            jumps += 1
-            index += curr_index
-        return jumps + 1
-                
+        ## at each point, iterate the full numebr of steps, at each point of the step project the max 
+
+        curr_pos = 0 ## for while loop pos
+        next_pos = 0 ## projected next pos
+        max_projection = 0 ## project next max step
+        num_steps = 0 ## final output
+
+        while curr_pos < len(nums) - 1:
+            if curr_pos + nums[curr_pos] >= len(nums) - 1:
+                return num_steps + 1
+            for step in range(1, nums[curr_pos] + 1):
+                if nums[curr_pos + step] + curr_pos + step > max_projection:
+                    max_projection = nums[curr_pos + step] + curr_pos + step
+                    next_pos = curr_pos + step
+            max_projection = 0
+            curr_pos = next_pos
+            num_steps += 1
+        return num_steps
