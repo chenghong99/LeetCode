d36721a25d6194b78328db4e8c9bfbcda41a6679
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-08-24 22:34:36 +0800

    Time: 171 ms (85.32%), Space: 29.7 MB (12.89%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.py
@@ -1,23 +1,29 @@
-import heapq
-class Solution:
-    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
-        
+class Solution(object):
+    def kSmallestPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        from heapq import heappush, heappop
+        heap = [(nums1[0] + nums1[0], (0, 0))]
         ans = []
-        heapq.heapify(ans)
+        visited = set()
+
+        while len(ans) < k:
+            val, (pos1, pos2) = heappop(heap)
+            ans.append([nums1[pos1], nums2[pos2]])
+
+            if pos1 + 1 < len(nums1) and (pos1 + 1, pos2) not in visited:
+                visited.add((pos1 + 1, pos2))
+                heappush(heap, (nums1[pos1 + 1] + nums2[pos2], (pos1 + 1, pos2)))
+
+            if pos2 + 1 < len(nums2) and (pos1, pos2 + 1) not in visited:
+                visited.add((pos1, pos2 + 1))
+                heappush(heap, (nums1[pos1] + nums2[pos2 + 1], (pos1, pos2 + 1)))
+
+        return ans
+
+
         
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                pairs = [nums1[i],nums2[j]]
-                if len(ans)<k:
-                    heapq.heappush(ans,[-(nums1[i]+nums2[j]),pairs])
-                elif nums1[i]+nums2[j]<-ans[0][0]:
-                        heapq.heappop(ans)
-                        heapq.heappush(ans,[-(nums1[i]+nums2[j]),pairs])
-                else:
-                    break
-        
-        res = []
-        for i in range(len(ans)):
-            res.append(ans[i][1])
-        
-        return res
