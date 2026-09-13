99c0b153d9c1617d6c31ff815f922c576816aa35
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-09-13 23:09:23 +0800

    Time: 372 ms (63.41%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,9 @@
-WITH raw AS (
-    SELECT
-    managerId
-    FROM Employee
-    GROUP BY 1
-    HAVING COUNT(id) >= 5)
-
-SELECT name from raw INNER JOIN Employee on raw.managerId = Employee.id
+WITH temp AS (SELECT 
+managerId
+FROM Employee
+GROUP BY 1
+HAVING COUNT(id) >= 5)
+SELECT
+name 
+FROM temp INNER JOIN Employee
+ON temp.managerId = Employee.id
