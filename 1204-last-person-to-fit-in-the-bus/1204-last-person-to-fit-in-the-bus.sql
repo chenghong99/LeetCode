4a8967cbfcd223eae041f2819dc135914613acec
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-09-13 20:54:37 +0800

    Time: 743 ms (96.8%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,7 +1,15 @@
-WITH pre_aggr AS (
+WITH temp AS (
     SELECT 
     person_id,
     person_name,
+    weight,
+    turn,
     SUM(weight) OVER (ORDER BY turn) AS cum_sum
     FROM Queue
-) SELECT person_name FROM pre_aggr WHERE cum_sum <= 1000 ORDER BY cum_sum DESC limit 1
+) 
+SELECT 
+person_name
+FROM temp
+WHERE cum_sum <= 1000 
+ORDER BY cum_sum DESC
+LIMIT 1
