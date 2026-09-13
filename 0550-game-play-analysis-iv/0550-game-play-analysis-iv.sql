91256cebd6c752bc23adf20b04fee3fd08d84797
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-09-13 21:08:11 +0800

    Time: 656 ms (40.76%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,14 +1,14 @@
 # Write your MySQL query statement below
-
-##count number of distinict player 
-## group by pyaer and get min start date 
-## select date where the start date is 1 day less 
-
-SELECT ROUND(COUNT(*) / (SELECT COUNT(DISTINCT a1.player_id)
-FROM Activity a1) , 2) AS fraction  
-FROM Activity a2
-WHERE (a2.player_id, DATE_SUB(a2.event_date, INTERVAL 1 DAY)) IN
-(SELECT a3.player_id, MIN(a3.event_date)
-FROM Activity a3
-GROUP BY a3.player_id)
-
+WITH min_date AS (
+    SELECT 
+    player_id,
+    MIN(event_date) AS first_login
+    FROM Activity
+    GROUP BY 1
+)
+SELECT 
+ROUND(COUNT(DISTINCT a.player_id)/ (SELECT COUNT(DISTINCT player_id) FROM Activity), 2) AS fraction
+FROM Activity a
+INNER JOIN min_date m 
+ON a.player_id = m.player_id 
+AND a.event_date = DATE_ADD(m.first_login, INTERVAL 1 DAY)
