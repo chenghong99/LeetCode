21c08242b263b3bb0c274cc27a8c6f072ad8016c
See Cheng Hong <84829074+chenghong99@users.noreply.github.com>
2026-09-24 12:12:16 +0800

    Time: 1047 ms (5.57%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,15 +1,18 @@
-WITH temp AS (
-    SELECT
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+## gaps and island. row number order by num - id  should be the same
+WITH raw AS (
+    SELECT 
     id, 
     num,
     ROW_NUMBER() OVER (PARTITION BY num ORDER BY id) AS row_num
     FROM Logs
-), raw AS (
-SELECT 
-num,
-id - row_num AS island
-FROM temp)
-SELECT DISTINCT num AS ConsecutiveNums
-FROM raw
+), temp AS (
+    SELECT 
+    num,
+    id - row_num AS island
+    FROM raw
+) 
+SELECT DISTINCT num AS ConsecutiveNums FROM temp
 GROUP BY num, island
 HAVING COUNT(*) >= 3
